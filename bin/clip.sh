c8b773639cf34c2338e42b0d33e0fb98244218e9
Melvin Carvalho <melvincarvalho@gmail.com>
2018-05-01 08:47:12 +0200

    Update main presentation

--- bin/clip.sh
+++ bin/clip.sh
@@ -7,3 +7,17 @@ cd $BIGDIR
 xclip -o | awk '{ print ; print "---" }' > $BIGDIR/index.md
 
 big-presentation-compose
+
+git add .
+
+git commit -m "Update main presentation"
+
+git push origin master
+
+git checkout gh-pages
+
+git merge master
+
+git push origin gh-pages
+
+git checkout master
